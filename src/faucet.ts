6fa20cc7fddb19dca0c39dae2a3f59cee0c6505d
Junho Yeo <i@junho.io>
2023-03-23 22:02:41 +0900

    Add support for Klaytn network

--- src/faucet.ts
+++ src/faucet.ts
@@ -14,12 +14,23 @@ const headers = {
   'sec-fetch-site': 'same-origin',
 };
 
-export const requestFaucet = async (account: string) => {
-  const { data } = await axios.post(
-    'https://wallet.test.wemix.com/faucet-api/',
-    { receiver: account },
-    { headers: headers },
-  );
-  console.log(data);
-  return data;
+export const requestFaucet = async (network: 'wemix' | 'klaytn', account: string) => {
+  if (network === 'wemix') {
+    const { data } = await axios.post(
+      'https://wallet.test.wemix.com/faucet-api/',
+      { receiver: account },
+      { headers: headers },
+    );
+    console.log(data);
+    return data;
+  }
+  if (network === 'klaytn') {
+    const { data } = await axios.post(
+      `https://api-baobab.wallet.klaytn.com/faucet/run?address=${account}`,
+      null,
+      { headers: headers },
+    );
+    console.log(data);
+    return data;
+  }
 };
